beac13dc10de79b04d94b86f998e0c4f2b0756bf
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-04-06 23:01:52 +0630

    medium banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Header from "../components/Header";
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +11,19 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+
+      <div className='bg-yellow-400 max-w-7xl mx-auto'>
+        <div className='px-10 space-y-5 border-y border-black py-10'>
+          <h1 className='text-6xl max-w-xl font-serif'>
+            <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read and connect
+          </h1>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore
+            accusamus reiciendis tempora eius officiis.
+          </h2>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
