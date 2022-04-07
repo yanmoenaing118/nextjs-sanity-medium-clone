b808f0bf31e982b5e99191fffb69a715772177f3
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-04-07 22:02:59 +0630

    comment form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -11,7 +11,7 @@ interface Props {
 export default function PostPage({ post }: Props) {
   console.log(post)
   return (
-    <main>
+    <main className='mb-12'>
       <Header />
       <img
         src={urlFor(post.mainImage).url()}
@@ -19,7 +19,7 @@ export default function PostPage({ post }: Props) {
         className="h-40 w-full object-cover md:h-80"
       />
 
-      <article className="mx-auto max-w-3xl px-5 md:p-0 mb-12">
+      <article className="mx-auto max-w-3xl px-5 md:p-0 ">
         <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
         <h2 className="mb-2 text-xl font-light text-gray-500">
           {post.description}
@@ -47,6 +47,32 @@ export default function PostPage({ post }: Props) {
           />
         </div>
       </article>
+
+      <hr  className='max-w-xl mx-auto border mt-10 border-yellow-400'/>
+
+      <form className='max-w-3xl mx-auto mt-12 flex flex-col p-5'>
+          <h3 className='font-bold text-yellow-500'>Enjoyed the article?</h3>
+          <h2 className='text-2xl font-bold'>Leave a comment below!</h2>
+          <hr className='my-4' />
+
+
+          <label className='my-2'>
+              <span className='font-bold text-gray-600'>Name</span>
+              <input type="text" className='block shadow border w-full py-2 px-3 rounded outline-none ring-yellow-500 focus:ring font-bold' />
+          </label>
+
+          <label className='my-2'>
+              <span className='font-bold text-gray-600'>Email</span>
+              <input type="email" className='block shadow border w-full py-2 px-3 rounded outline-none ring-yellow-500 focus:ring font-bold' />
+          </label>
+
+          <label className='my-2'>
+              <span className='font-bold text-gray-600'>Comment</span>
+              <textarea rows={8}  className='resize-none block shadow border w-full py-2 px-3 rounded outline-none ring-yellow-500 focus:ring font-bold'/>
+          </label>
+
+          <button type="submit" className='w-full bg-yellow-500 text-white py-2 hover:bg-yellow-400 mt-4'>submit</button>
+      </form>
     </main>
   )
 }
