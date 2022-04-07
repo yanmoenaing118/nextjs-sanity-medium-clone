9b143f6b26454d1de44625f8b3d4cadd88fe0340
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-04-07 20:08:13 +0630

    post paths

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,77 @@
+import { sanityClient, urlFor } from '../../sanity'
+import Header from '../../components/Header'
+import { Post } from './../../typings'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+interface Props {
+  post: Post
+}
+
+export default function PostPage({ post }: Props) {
+  console.log(post)
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const query = `*[type=='post'] {
+        _id,
+        slug {
+            current
+        }
+    }`
+
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `
+    *[_type=='post' && slug.current == $slug][0] {
+        _id,
+        _createdAt,
+        title,
+        author->{
+            name,
+            image
+        },
+        'comments': *[
+            _type == 'comment' &&
+            post._ref == ^._id &&
+            approved == true
+        ],
+        description,
+        mainImage,
+        slug,
+        body
+    }
+  `
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+  }
+}
