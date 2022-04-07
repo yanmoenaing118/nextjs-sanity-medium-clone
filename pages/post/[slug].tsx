40bbb2dbf87aad1fa9bb928535ccec292a32e8bf
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-04-07 21:07:38 +0630

    post article

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,6 +2,7 @@ import { sanityClient, urlFor } from '../../sanity'
 import Header from '../../components/Header'
 import { Post } from './../../typings'
 import { GetStaticProps, GetStaticPaths } from 'next'
+import PortableText from 'react-portable-text'
 
 interface Props {
   post: Post
@@ -12,6 +13,40 @@ export default function PostPage({ post }: Props) {
   return (
     <main>
       <Header />
+      <img
+        src={urlFor(post.mainImage).url()}
+        alt={post.title}
+        className="h-40 w-full object-cover md:h-80"
+      />
+
+      <article className="mx-auto max-w-3xl px-5 md:p-0 mb-12">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-5">
+          <img
+            src={urlFor(post.author.image).url()}
+            alt={`Author: ${post.author.name}`}
+            className="h-12 w-12 rounded-full object-cover"
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at &nbsp;
+            {new Date(post._createdAt).toLocaleDateString()}
+          </p>
+        </div>
+
+        <div className='mt-10'>
+          <PortableText
+            content={post.body}
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+          />
+        </div>
+      </article>
     </main>
   )
 }
@@ -73,5 +108,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    revalidate: 60,
   }
 }
